74a750fa1ec3acf4264ef8c0663fc55987eb6cf3
Usumez <usumez74@gmail.com>
2021-12-20 12:58:29 +0000

    New Trials

---
+++ src/test/java/newFeature/NewFeatureTest.java
@@ -0,0 +1,12 @@
+package newFeature;
+
+public class NewFeatureTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("I am making some trial changes");
+
+    }
+
+
+}
